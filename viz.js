75ff568031fec173963b49db3b605ecd1c4f2da0
ZaMan <21971457+Zaman8@users.noreply.github.com>
2024-01-07 13:50:36 -0500

    remove ref to no longer used boolean

--- viz.js
+++ viz.js
@@ -1,14 +1,14 @@
 const audioViz = document.getElementById('AudioViz');
 
 
-//Hande setting Audio info Display
+//Handle setting Audio info Display
 
 const albumCover = document.getElementById('albumPic');
 
 function livelyCurrentTrack(data) {
     let obj = JSON.parse(data);
     //when no track is playing its null
-    if (obj != null && showAudio)
+    if (obj != null)
     {
       audioViz.style.visibility = "visible";
       if (obj.Thumbnail != null)
