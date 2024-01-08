1512091a49a04b5b549fe38f8f3f5b9db01ac4e4
ZaMan <21971457+Zaman8@users.noreply.github.com>
2024-01-07 23:40:11 -0500

    functioning audio visualizer

---
+++ musichandler.js
@@ -0,0 +1,87 @@
+const audioViz = document.getElementById('AudioViz');
+
+
+//Handle setting Audio info Display visible 
+
+const albumCover = document.getElementById('sourceImage');
+
+function livelyCurrentTrack(data) {
+    let obj = JSON.parse(data);
+    //when no track is playing its null
+    if (obj != null)
+    {
+      audioViz.style.visibility = "visible";
+      if (obj.Thumbnail != null)
+      {
+        albumCover.src = "data:image/png;base64, " + obj.Thumbnail;
+      }
+    } else {
+      audioViz.style.visibility = "hidden";
+      albumCover.src = "placeholderAlbumArt.jpg"
+    }
+  }
+
+let canvas = document.getElementById("visualizer");
+let max_height, startPos, vizWidth, midY;
+
+let linesColor = "rgb(255,255,255)";
+let backgroundColor = "rbga(0,0,0,0)"
+let square = false;
+
+let ctx = canvas.getContext("2d");
+
+function setSize() {
+  max_height = canvas.height * 0.5;
+  startPos = canvas.width * 0.1;
+  vizWidth = canvas.width * 0.8;
+  midY = canvas.height - canvas.height / 4;
+  gradient = ctx.createLinearGradient(0,midY, 0, max_height);
+  gradient.addColorStop(0, backgroundColor);
+  gradient.addColorStop(1, linesColor);
+
+}
+
+window.onload = () => {
+  setSize();
+};
+
+function livelyAudioListener(audioArray) 
+{
+  maxVal = 1;
+  for (var x of audioArray) {
+    if (x > maxVal) maxVal = x;
+  }
+
+  const offSet = vizWidth / audioArray.length;
+  ctx.fillstyle = backgroundColor;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.beginPath();
+  ctx.lineJoin = "round";
+  ctx.moveTo(startPos - offSet * 3, midY);
+  ctx.lineTo(startPos, midY);
+  let posInLine = -1;
+  for (var x = 0; x < audioArray.length; x++) {
+    posInLine++;
+    ctx.lineTo(
+      startPos + offSet * posInLine,
+      midY - (audioArray[x] / maxVal) * max_height
+    );
+    if (square)
+      ctx.lineTo(
+        startPos + offSet * (posInLine + 1),
+        midY - (audioArray[x] / maxVal) * max_height
+      );
+  }
+  ctx.lineTo(startPos + offSet * (posInLine + (square ? 1 : 0)), midY);
+  ctx.lineTo(startPos + offSet * (posInLine + (square ? 4 : 3)), midY);
+
+  ctx.fill();
+  renderLine(linesColor);
+}
+
+function renderLine(color) {
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
